93885e87203d963a422a4ee9b5e89c389d18ff7d
Piri <johannes@pieringer.one>
2020-10-05 21:44:39 +0200

    Draw all relationships

--- src/main/java/one/pieringer/javaquery/plantuml/PlantUmlTransformer.java
+++ src/main/java/one/pieringer/javaquery/plantuml/PlantUmlTransformer.java
@@ -1,15 +1,18 @@
 package one.pieringer.javaquery.plantuml;
 
-import one.pieringer.javaquery.database.ResultSet;
-import one.pieringer.javaquery.model.FieldRelationship;
-import one.pieringer.javaquery.model.InheritanceRelationship;
-import one.pieringer.javaquery.model.Type;
-
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+
+import one.pieringer.javaquery.database.ResultSet;
+import one.pieringer.javaquery.model.AccessFieldRelationship;
+import one.pieringer.javaquery.model.CreateInstanceRelationship;
+import one.pieringer.javaquery.model.FieldRelationship;
+import one.pieringer.javaquery.model.InheritanceRelationship;
+import one.pieringer.javaquery.model.InvokeRelationship;
+import one.pieringer.javaquery.model.Type;
 
 public class PlantUmlTransformer {
     public static final String START_UML = "@startuml\n";
@@ -27,6 +30,18 @@ public class PlantUmlTransformer {
 
         for (FieldRelationship fieldRelationship : resultSet.getFieldRelationships()) {
             uml.append(transform(fieldRelationship));
+        }
+
+        for (CreateInstanceRelationship createInstanceRelationship : resultSet.getCreateInstanceRelationships()) {
+            uml.append(transform(createInstanceRelationship));
+        }
+
+        for (AccessFieldRelationship accessFieldRelationship : resultSet.getAccessFieldRelationships()) {
+            uml.append(transform(accessFieldRelationship));
+        }
+
+        for (InvokeRelationship invokeRelationship : resultSet.getInvokeRelationships()) {
+            uml.append(transform(invokeRelationship));
         }
 
         for (InheritanceRelationship inheritanceRelationship : resultSet.getInheritanceRelationships()) {
@@ -56,8 +71,46 @@ public class PlantUmlTransformer {
         StringBuilder uml = new StringBuilder();
         uml.append(fieldRelationship.getContainingType().getName());
         uml.append(" ");
-        uml.append("--> ");
+        uml.append("-[bold]-> ");
         uml.append(fieldRelationship.getFieldType().getName());
+        uml.append(" ");
+        uml.append(": has-field");
+        uml.append("\n");
+        return uml;
+    }
+
+    private StringBuilder transform(@Nonnull final CreateInstanceRelationship createInstanceRelationship) {
+        StringBuilder uml = new StringBuilder();
+        uml.append(createInstanceRelationship.getContainingType().getName());
+        uml.append(" ");
+        uml.append("-[plain]-> ");
+        uml.append(createInstanceRelationship.getObjectType().getName());
+        uml.append(" ");
+        uml.append(": creates");
+        uml.append("\n");
+        return uml;
+    }
+
+    private StringBuilder transform(@Nonnull final AccessFieldRelationship accessFieldRelationship) {
+        StringBuilder uml = new StringBuilder();
+        uml.append(accessFieldRelationship.getContainingType().getName());
+        uml.append(" ");
+        uml.append("-[dashed]-> ");
+        uml.append(accessFieldRelationship.getFieldDeclaringType().getName());
+        uml.append(" ");
+        uml.append(": accesses");
+        uml.append("\n");
+        return uml;
+    }
+
+    private StringBuilder transform(@Nonnull final InvokeRelationship invokeRelationship) {
+        StringBuilder uml = new StringBuilder();
+        uml.append(invokeRelationship.getContainingType().getName());
+        uml.append(" ");
+        uml.append("-[dotted]-> ");
+        uml.append(invokeRelationship.getInvokedType().getName());
+        uml.append(" ");
+        uml.append(": invokes");
         uml.append("\n");
         return uml;
     }
@@ -68,6 +121,8 @@ public class PlantUmlTransformer {
         uml.append(" ");
         uml.append("--|> ");
         uml.append(inheritanceRelationship.getSuperType().getName());
+        uml.append(" ");
+        uml.append(": inherits");
         uml.append("\n");
         return uml;
     }
